24c42da4ff9c1533e0d2db55adb342527a772dd5
shevchenk <jorgeshevchenk@gmail.com>
2017-01-14 00:39:19 -0500

    Primera subida

---
+++ registro.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>.::Telesup::.</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+</head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+  <header class="main-header">
+    <nav class="navbar navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+        <a href="../../index2.html" class="navbar-brand">.::TELESUP::.</a>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+          </button>
+        </div>
+
+        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#"><b>CONCURSO NACIONAL DE DOCENTES POR INVITACIÓN</b></a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <div class="content-wrapper">
+    <div class="container">
+      <section class="content-header">
+        <h1>
+          
+        </h1>
+      </section>
+
+      <section class="content">
+        <div class="row">
+        <div class="col-md-6">
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <i class="fa fa-indent"></i>
+              <h3 class="box-title">DATOS DEL POSTULANTE</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <form role="form">
+                <div class="box-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">DNI:</label>
+                    <input type="text" class="form-control" id="txt_dni" placeholder="Ingrese DNI">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Paterno</label>
+                    <input type="text" class="form-control" id="txt_paterno" placeholder="Ingrese Paterno">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Materno</label>
+                    <input type="text" class="form-control" id="txt_materno" placeholder="Ingrese Materno">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Nombre(s)</label>
+                    <input type="text" class="form-control" id="txt_nombres" placeholder="Ingrese Nombres">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <input type="file" id="exampleInputFile">
+
+                    <p class="help-block">Example block-level help text here.</p>
+                  </div>
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> Check me out
+                    </label>
+                  </div>
+                </div>
+                <!-- /.box-body -->
+
+                <div class="box-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
+      </section>
+    </div>
+  </div>
+
+  <footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+      </div>
+      <strong>Copyright &copy; 2017</strong> All rights reserved.
+    </div>
+  </footer>
+</div>
+
+<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="plugins/fastclick/fastclick.js"></script>
+<script src="dist/js/app.min.js"></script>
+<script src="dist/js/demo.js"></script>
+</body>
+</html>
